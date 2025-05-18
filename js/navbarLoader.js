c3ec49621dedfc6662a701941fe5cd5c1c4b23d3
miltonayala <miltonayala1704@gmail.com>
2025-05-17 23:23:12 -0600

    made navbar a single item with JS fetch

---
+++ js/navbarLoader.js
@@ -0,0 +1,23 @@
+// navbar.js
+fetch('navbar.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('menu-container').innerHTML = data;
+
+    // Esperamos a que el DOM del navbar cargue antes de añadir el listener
+    const waitForToggle = () => {
+      const toggle = document.getElementById('menu-toggle');
+      const menu = document.getElementById('menu');
+
+      if (toggle && menu) {
+        toggle.addEventListener('click', () => {
+          menu.classList.toggle('show');
+        });
+      } else {
+        // Si no está disponible aún, lo intentamos de nuevo pronto
+        setTimeout(waitForToggle, 50);
+      }
+    };
+
+    waitForToggle();
+  });
